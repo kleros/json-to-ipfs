6c7a134c51290c0d1a3c958efb7d7cd77f68ba29
Matheus Alencar <malencar@pm.me>
2020-01-16 15:13:11 -0300

    feat: initial release

---
+++ README.md
@@ -0,0 +1,22 @@
+<p align="center">
+  <b style="font-size: 32px;">JSON to IPFS</b>
+</p>
+
+<p align="center">
+  <a href="https://standardjs.com"><img src="https://img.shields.io/badge/code_style-standard-brightgreen.svg" alt="JavaScript Style Guide"></a>
+  <a href="https://conventionalcommits.org"><img src="https://img.shields.io/badge/Conventional%20Commits-1.0.0-yellow.svg" alt="Conventional Commits"></a>
+  <a href="http://commitizen.github.io/cz-cli/"><img src="https://img.shields.io/badge/commitizen-friendly-brightgreen.svg" alt="Commitizen Friendly"></a>
+  <a href="https://app.netlify.com/sites/ecstatic-jackson-749344/deploys"><img src="https://api.netlify.com/api/v1/badges/ff0eb1e7-e70c-4319-9e5c-f8532b053900/deploy-status"></a>
+</p>
+
+## Usage
+
+1.  Clone this repo;
+2.  Duplicate `.env.example` and rename it to `.env`. Fill the IPFS_GATEWAY variables.
+3.  Edit the add fields to the object assigned to `file` inside `index.js`.
+4.  Run `yarn` to install dependencies and then `yarn start` to upload.
+
+## Other Scripts
+
+- `yarn format` - Lint, fix and prettify all the project.
+- `yarn run cz` - Run commitizen.
---
+++ index.js
@@ -0,0 +1,54 @@
+if (!process.env.IPFS_GATEWAY) {
+  console.error(
+    'Please set the environment variable IPFS_GATEWAY to a valid IPFS endpoint.'
+  )
+  throw new Error('IPFS Gateway not set.')
+}
+
+const fetch = require('node-fetch')
+const Archon = require('@kleros/archon')
+const TextEncoder = require('text-encoder-lite').TextEncoderLite
+
+/**
+ * Send file to an IPFS node.
+ * @param {string} fileName - The name that will be used to store the file. This is useful to preserve extension type.
+ * @param {ArrayBuffer} data - The raw data from the file to upload.
+ * @returns {object} IPFS response. Includes the hash and path of the stored item.
+ */
+const ipfsPublish = async (fileName, data) => {
+  const buffer = Buffer.from(data)
+
+  return new Promise((resolve, reject) => {
+    fetch(`${process.env.IPFS_GATEWAY}/add`, {
+      method: 'POST',
+      body: JSON.stringify({
+        fileName,
+        buffer
+      }),
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(success => resolve(success.data))
+      .catch(err => reject(err))
+  })
+}
+
+;(async () => {
+  // Edit here. ---------
+
+  const file = {} // JSON to be uploaded.
+
+  // --------------------
+
+  const fileData = new TextEncoder('utf-8').encode(JSON.stringify(file))
+  /* eslint-disable-next-line unicorn/number-literal-case */
+  const fileMultihash = Archon.utils.multihashFile(file, 0x1b)
+
+  console.info('Uploading to ipfs...')
+  const ipfsObject = await ipfsPublish(fileMultihash, fileData)
+  const ipfsPath = `/ipfs/${ipfsObject[1].hash + ipfsObject[0].path}`
+
+  console.info(`File available at ${ipfsPath}`)
+})()
